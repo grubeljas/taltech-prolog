3edb8415c509281d5581bba0f26f26d5b0b021fc
pavel.grubeljas <pavel.grubeljas@gamesys.ee>
2022-09-20 16:51:55 +0300

    03 paki

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -3,9 +3,18 @@ viimane_element(El, [El]).
 viimane_element(El, [_|TAIL]):-
     viimane_element(El, TAIL).
 
+suurim([], []).
 suurim([El|[]], [El]).
 suurim([X,Y|TAIL], [B|L]):-
     ((X > Y)
     -> B = X
     ; B = Y)
     ,suurim([Y|TAIL], L).
+
+paki([], []).
+paki([X,X|[]], [X]).
+paki([X|[]], [X]).
+paki([X,X|TAIL], [X|L]):-
+    paki(TAIL, L).
+paki([X,Y|TAIL], [X|L]):-
+    paki([Y|TAIL], [Y|L]).
