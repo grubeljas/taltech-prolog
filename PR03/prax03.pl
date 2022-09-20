cb46ef1ee0876bfc3262dd47769158b0913aca2d
pavel.grubeljas <pavel.grubeljas@gamesys.ee>
2022-09-20 16:28:00 +0300

    03 suurim

--- PR03/prax03.pl
+++ PR03/prax03.pl
@@ -3,4 +3,9 @@ viimane_element(El, [El]).
 viimane_element(El, [_|TAIL]):-
     viimane_element(El, TAIL).
 
-suurim([], X):- fail.
+suurim([El|[]], [El]).
+suurim([X,Y|TAIL], [B|L]):-
+    ((X > Y)
+    -> B = X
+    ; B = Y)
+    ,suurim([Y|TAIL], L).
