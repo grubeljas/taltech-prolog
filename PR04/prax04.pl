28f29557cc66f28d47a3b25b665cff0dfa2ded0d
pavel.grubeljas <pavel.grubeljas@gamesys.ee>
2022-09-27 16:42:53 +0300

    04 ring

---
+++ PR04/prax04.pl
@@ -0,0 +1,11 @@
+:- dynamic labitud/1.
+
+laevaga(tallinn, helsinki, 120).
+laevaga(tallinn, stockholm, 480).
+bussiga(tallinn, riia, 300).
+rongiga(riia, berlin, 680).
+lennukiga(tallinn, helsinki, 30).
+lennukiga(helsinki, paris, 180).
+lennukiga(paris, berlin, 120).
+lunnukiga(berlin, stockholm, 80).
+lennukiga(stockholm, tallinn, 50).
