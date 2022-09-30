faca31fa2c4676ba885da4a2f14249a28348649c
Pavel Grubeljas <pagrub@ttu.ee>
2022-09-30 15:25:42 +0300

    04

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -34,22 +34,36 @@ reisi(Kust, Kuhu, mine(Kust, Peatus,Road)):-
     reisi(Peatus, Kuhu, Road),
     retractall(labitud(_)).
 
-transport(Kust, Kuhu, laevaga):-
-    laevaga(Kust, Kuhu,_).
-transport(Kust, Kuhu, bussiga):-
-    bussiga(Kust, Kuhu,_).
-transport(Kust, Kuhu, rongiga):-
-    rongiga(Kust, Kuhu,_).
-transport(Kust, Kuhu, lennukiga):-
-    lennukiga(Kust, Kuhu,_).
+transport(Kust, Kuhu, laevaga, N):-
+    laevaga(Kust, Kuhu, N).
+transport(Kust, Kuhu, bussiga, N):-
+    bussiga(Kust, Kuhu, N).
+transport(Kust, Kuhu, rongiga, N):-
+    rongiga(Kust, Kuhu, N).
+transport(Kust, Kuhu, lennukiga, N):-
+    lennukiga(Kust, Kuhu, N).
 reisi_transpordiga(Kust, Kuhu, mine(Kust, Kuhu, Transport)):-
-    transport(Kust, Kuhu, Transport).
+    transport(Kust, Kuhu, Transport, _).
 reisi_transpordiga(Kust, Kuhu, mine(Kust, Peatus, Transport, mine(Peatus,Kuhu,Transport1))):-
-    transport(Kust, Peatus, Transport),
+    transport(Kust, Peatus, Transport, _),
     reisi_transpordiga(Peatus, Kuhu, mine(Peatus, Kuhu, Transport1)).
 reisi_transpordiga(Kust, Kuhu, mine(Kust, Peatus, Transport, Road)):-
     \+ labitud(Peatus),
-    transport(Kust, Peatus, Transport),
+    transport(Kust, Peatus, Transport, _),
     assert(labitud(Peatus)),
     reisi_transpordiga(Peatus, Kuhu, Road);
+    retractall(labitud(_)).
+
+reisi(Kust, Kuhu, mine(Kust, Kuhu, Transport), N):-
+    transport(Kust, Kuhu, Transport, N).
+reisi(Kust, Kuhu, mine(Kust, Peatus, Transport, mine(Peatus,Kuhu,Transport1)), N):-
+    transport(Kust, Kuhu, Transport, N1),
+    reisi(Peatus, Kuhu, mine(Peatus, Kuhu, Transport1), N2),
+    N is N1 + N2.
+reisi(Kust, Kuhu, mine(Kust, Peatus, Transport, Road), N):-
+    \+ labitud(Peatus),
+    transport(Kust, Kuhu, Transport, N1),
+    assert(labitud(Peatus)),
+    reisi(Peatus, Kuhu, Road, N2),
+    N is N1 + N2;
     retractall(labitud(_)).
