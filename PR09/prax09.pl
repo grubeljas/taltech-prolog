cb58d1ac12398dab0f08c1636740d74265c0be5b
Pavel Grubeljas <pagrub@ttu.ee>
2022-11-08 21:16:16 +0200

    09 test

---
+++ PR09/prax09.pl
@@ -0,0 +1,10 @@
+lihtlause --> nimisonafraas, tegusonafraas.
+nimisonafraas --> nimisona, omadussonafraas, nimisona.
+nimisonafraas --> nimisona,nimisonafraas ;[].
+nimisona -->[pakapiku];[habe];[tema];[sobimatuse];[jouluvanaks]. % terminalsümbolid esinevad reeglis paremal pool ühiklistidena
+omadussonafraas --> maarsona, omadussona.
+maarsona --> [liiga].
+omadussona --> [lyhike].
+tegusonafraas --> tegusona, nimisonafraas.
+tegusona --> [tingib];[pohjustab].
+% test
