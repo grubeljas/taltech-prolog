04f374b7d2324d82a3fb221d99723730461faad7
Pavel Grubeljas <pagrub@ttu.ee>
2022-11-28 02:50:04 +0200

    11 yhisosa

--- PR11/prax11.pl
+++ PR11/prax11.pl
@@ -4,5 +4,5 @@ yhisosa([El|Tale], List1, [El|X]):-
     member(El,List1),
     yhisosa(Tale,List1,X).
 yhisosa([El|Tale], List1, X):-
-    not member(El,List1),
+    \+member(El,List1),
     yhisosa(Tale,List1,X).
