d96285ce5c502e7f2f27c8b4a6c320e758e7c43c
pavel.grubeljas <pavel.grubeljas@gamesys.ee>
2022-11-28 15:53:32 +0200

    11

--- PR11/prax11.pl
+++ PR11/prax11.pl
@@ -9,12 +9,12 @@ yhisosa([El|Tale], List1, X):-
 
 yhend(List,[],List).
 yhend(List, [El|Tale], X):-
-    member(El, X),
+    member(El, List),
     yhend(List, Tale, X).
-yhend(List, [EL|Tale], X):-
-    \+member(El, X),
-    append(X1, [El], X),
-    yhend(List, Tale, X1).
+yhend(List, [El|Tale], X):-
+    \+member(El, List),
+    yhend(List, Tale, X1),
+    append(X1, [El], X).
 
 vahe([],_,[]).
 vahe([El|Tale],B,X):-
@@ -22,6 +22,6 @@ vahe([El|Tale],B,X):-
     yhend(Tale,B,X).
 vahe([El|Tale],B,X):-
     \+member(El,B),
-    append(X1,[EL],X),
+    append(X1,[El],X),
     yhend(Tale,B,X1).
 
